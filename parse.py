061c2e558610c20cd551a34897410c97f2307b5e
monk-time <monk-time@users.noreply.github.com>
2017-07-31 17:07:42 +0300

    Finish data parsing

--- parse.py
+++ parse.py
@@ -1,61 +1,90 @@
-"""Use this for collecting and cleaning data from fresh .xls tables from TSPDT site.
+"""Use this for collating the data from three different places on TSPDT.
 
-All tables must be converted to .csv manually before use.
-In Excel choose 'Unicode Text (.txt)' (tab-separated, UTF16-LE),
+All tables must be converted to .csv manually before use:
+in Excel click 'Save as -> Unicode Text (.txt)' (tab-separated, UTF16-LE),
 that's the only way to preserve unicode chars in Excel.
-"""
+Rename files to .csv after exporting.
+
+The third file with actual ranks comes from a JS-tool that scrapes director pages."""
 
 import csv
 import re
 from collections import OrderedDict
-from itertools import groupby
-from operator import itemgetter
-from typing import Dict, Iterable, List, Mapping
+from typing import Callable, Dict, Iterable, List, Mapping, Optional
 
 PATH_NEXT1K = 'data/Films-Ranked-1001-2000.csv'
 PATH_YEARLY_TOP25 = 'data/Yearly-Top-25s-GF1000.csv'
+PATH_DIRECTORS = 'data/directors_scraped.csv'
 
 
-def parse_tsv(filename) -> List[Dict]:
-    """Opens a .csv file exported from Excel."""
-    with open(filename, encoding="utf-16") as f:
-        cfg = {'dialect': 'excel-tab'}
-        rows = list(csv.DictReader(f, **cfg))
+def parse_tsv(filename: str, row_modifier: Callable) -> List[Dict]:
+    """Open a .csv file exported from Excel, applying a modifier function to each row.
+    Falsy rows are skipped."""
+    with open(filename, encoding='utf-16') as f:
+        rows = list(filter(None, map(row_modifier, csv.DictReader(f, dialect='excel-tab'))))
     return rows
 
 
-def extract_title(row: dict) -> str:
-    title_full = row['Title/Year/Country/Length/Colour']
-    # sometimes a year is not the first value after a bracket
+def pick(keys: Iterable, d: Mapping, f: Callable = lambda x: x) -> dict:
+    """Return a dictionary only with provided keys, optionally passed through a given function."""
+    return {key: f(d[key]) for key in keys if key in d}
+
+
+def mod_next1k(row: Mapping) -> OrderedDict:
+    return OrderedDict({
+        'Year': int(re.match('\d{4}', row['Year']).group()),  # take the lower year from ranges
+        'Title': row['Title'],
+        'Director': normalize_directors(row['Director'])})
+
+
+def mod_dirs(row: Mapping) -> OrderedDict:
+    return OrderedDict(**pick(['Pos', 'Year'], row, int),
+                       **pick(['Title', 'Director'], row))
+
+
+def mod_yearly(row: Mapping) -> Optional[OrderedDict]:
+    return OrderedDict({
+        'Yearly Pos': int(row['Pos']),
+        'Year': int(row['Year']),
+        'Title': extract_title(row['Title/Year/Country/Length/Colour']),
+        'Director': normalize_directors(row['Director'])
+    }) if row['Overall Pos'] == '1001-2000' else None
+
+
+def extract_title(field: str) -> str:
+    # Sometimes a year is not the first value after a bracket
     title = re.fullmatch(r"""
         \s*(.+)\s+          # film title; trim whitespace
         \((
-           .*\b\d{4},\s.*   # extra info in parens, incl. year
-        )\)
-        """, title_full, re.VERBOSE)
+           .*\b\d{4}.*   # extra info in parens, incl. year
+        )\)\s*
+        """, field, re.VERBOSE)
     if not title:
-        raise AssertionError("Can't extract film title from this row:\n    {}"
-                             .format(title_full))
+        raise ValueError(f"Can't extract film title from this row:\n    {field}")
     return title.group(1)
 
 
-def pick(keys: Iterable, d: Mapping) -> dict:
-    """Returns a dictionary with only provided keys."""
-    return {key: d[key] for key in keys if key in d}
+def normalize_directors(s: str) -> str:
+    """Restore proper first-last name order and use a uniform separator between multiple names."""
+    def flip(name):
+        return ' '.join(reversed(name.split(', ')))
+
+    return ' & '.join(map(flip, re.split(' & |/', s)))
 
 
-yearly = parse_tsv(PATH_YEARLY_TOP25)
-# pick only relevant rows and fields and add a clean title
-yearly1k = ({'Title': extract_title(r), **pick(['Year', 'Director', 'Pos'], r)}
-            for r in yearly if r['Overall Pos'] == '1001-2000')
-# group by year
-iter_g = groupby(yearly1k, itemgetter('Year'))
-yearly1k = OrderedDict((int(year), [pick(['Director', 'Title'], r) for r in group])
-                       for year, group in iter_g)
+def main():
+    next1k = parse_tsv(PATH_NEXT1K, mod_next1k)
+    # pprint(next1k, width=200)
+    print(f'Next1k: {len(next1k)}')
 
-# pprint(yearly1k, width=150)
-next1k = [pick(['Title', 'Director', 'Year'], r) for r in parse_tsv(PATH_NEXT1K)]
-for year, group in yearly1k.items():
-    print('{}:'.format(year))
-    for pos, r in enumerate(group):
-        print("    #{:>2}: {r[Title]:50} - {r[Director]}".format(pos + 1, r=r))
+    dirs = parse_tsv(PATH_DIRECTORS, mod_dirs)
+    # pprint(dirs, width=200)
+    print(f'Directors: {len(dirs)}')
+
+    yearly = parse_tsv(PATH_YEARLY_TOP25, mod_yearly)
+    # pprint(yearly, width=200)
+    print(f'Yearly: {len(yearly)}')
+
+
+if __name__ == '__main__':
+    main()
